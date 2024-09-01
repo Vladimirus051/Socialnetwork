e9205d22f269c2e4ef2325dba47b20ae4e65a1e1
VleekV <vova.shanya@yandex.ru>
2024-09-01 13:52:48 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ let AppContainer = compose(
     connect(mapStateToProps, {initializeApp}))(App);
 
 const MainApp = (props) => {
-    return <HashRouter basename={process.env.PUBLIC_URL}>
+    return <HashRouter>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
